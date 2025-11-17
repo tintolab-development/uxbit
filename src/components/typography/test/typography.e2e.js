456e0b63c441aeabd50039c02089b05fc204536b
keonhee <2kunhee94@gmail.com>
2025-11-17 15:48:34 +0900

    [삭제] 불필요 js 파일 삭제

--- src/components/typography/test/typography.e2e.js
+++
@@ -1,9 +0,0 @@
-import { newE2EPage } from '@stencil/core/testing';
-describe('hello-uxbit', () => {
-  it('renders', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<hello-uxbit></hello-uxbit>');
-    const element = await page.find('hello-uxbit');
-    expect(element).toHaveClass('hydrated');
-  });
-});
